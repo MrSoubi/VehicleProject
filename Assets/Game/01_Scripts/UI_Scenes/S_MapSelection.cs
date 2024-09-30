35a52f6fb078b56317558167a594c9fcb338ab75
N3crom <mathias.manenq@gmail.com>
2024-09-30 16:59:24 +0200

    Finsh the map Selection

--- Assets/Game/01_Scripts/UI_Scenes/S_MapSelection.cs
+++ Assets/Game/01_Scripts/UI_Scenes/S_MapSelection.cs
@@ -4,55 +4,79 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using UnityEngine.UIElements;
 
 public class S_MapSelection : MonoBehaviour
 {
     [SerializeField] private GameObject[] _maps;
-    //[SerializeField] private float slideSpeed = 5f;
     [SerializeField] private PlayersData _playerData;
+    private Vector3 _firstMapPosition = new Vector3(-5f, -1, -10f);
+    private Vector3 _secondMapPosition = new Vector3(7.5f, -1f, 5.5f);
+    private Vector3 _mapBeforeposition = new Vector3(-17.5f, -1f, 5.5f);
+    private float _distanceXBetweenMap = 15f;
     private Dictionary<InputDevice, PlayerInfo> _players => _playerData.players;
 
 
-    private int currentMapIndex = 0;
+    private int _currentMapIndex = 0;
 
     private void Start()
     {
-               //UpdateMapDisplay();
+        _maps[0].transform.position = _firstMapPosition;
+        _maps[1].transform.position = _secondMapPosition;
+        for (int i = 2; i < _maps.Length; i++)
+        {
+            _maps[i].transform.position = new Vector3 (_secondMapPosition.x + _distanceXBetweenMap * (i - 1), _secondMapPosition.y, _secondMapPosition.z);
+        }
+
+        Debug.Log(_maps.Length);
     }
 
     private void NextMap()
     {
-        currentMapIndex++;
-        if (currentMapIndex >= _maps.Length)
+        
+        _currentMapIndex++;
+       
+
+        foreach (var map in _maps)
         {
-            currentMapIndex = 0;
+            map.transform.position = new Vector3(map.transform.position.x - _distanceXBetweenMap, map.transform.position.y);
         }
-        //UpdateMapDisplay();
+
+        _maps[_currentMapIndex].transform.position = _firstMapPosition;
+
+        if (_currentMapIndex < _maps.Length - 1)
+        {
+            _maps[_currentMapIndex + 1].transform.position = _secondMapPosition;
+        }
+        else
+        {
+            Debug.Log("out of map");
+        }
     }
 
     private void PreviousMap()
     {
-        currentMapIndex--;
-        if (currentMapIndex < 0)
-        {
-            currentMapIndex = _maps.Length - 1;
-        }
-        //UpdateMapDisplay();
-    }
-
-    private void UpdateMapDisplay()
-    {
+        _currentMapIndex--;
         
         foreach (var map in _maps)
         {
-            map.SetActive(false);
+            map.transform.position = new Vector3(map.transform.position.x + _distanceXBetweenMap, map.transform.position.y);
         }
 
-        
-        _maps[currentMapIndex].SetActive(true);
+        _maps[_currentMapIndex].transform.position = _firstMapPosition;
 
-        Debug.Log("current map: " + currentMapIndex);
+        if (_currentMapIndex - 1 >= 0)
+        {
+            _maps[_currentMapIndex - 1].transform.position = _mapBeforeposition;
+        }
+        else
+        {
+            Debug.Log("out of map");
+        }
+        
     }
+
+    
 
     private void DisableOtherPlayersControls()
     {
@@ -73,15 +97,15 @@ public class S_MapSelection : MonoBehaviour
         if (context.performed)
         {
             float input = context.ReadValue<float>();
-            if (input > 0)
+            if (input > 0 && _currentMapIndex < _maps.Length - 1)
             {
                 NextMap();
-                ChangeMapDisplay(true);
+                Debug.Log("current map: " + _currentMapIndex);
             }
-            else
+            else if (input < 0 && _currentMapIndex > 0)
             {
                 PreviousMap();
-                ChangeMapDisplay(false);
+                Debug.Log("current map: " + _currentMapIndex);
             }
         }
     }
@@ -91,7 +115,7 @@ public class S_MapSelection : MonoBehaviour
         if (context.performed)
         {
             ChangeActionMap();
-            SceneManager.LoadScene("Map_"+ currentMapIndex.ToString());//Load scene to test
+            SceneManager.LoadScene("Map_"+ _currentMapIndex.ToString());//Load scene to test
         }
     }
 
@@ -104,10 +128,7 @@ public class S_MapSelection : MonoBehaviour
 
     }
 
-    public void ChangeMapDisplay(bool switchRight)
-    {
-
-    }
+    
 
     public void ChangeActionMap()
     {
