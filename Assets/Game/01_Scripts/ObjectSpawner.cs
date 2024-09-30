4cf95b897cf7fb5618b40ab74d0acb994381cd7a
MartinsFlorian <f.martins2005.fm@gmail.com>
2024-09-30 11:09:00 +0200

    ObjectSpawner script + prefab

---
+++ Assets/Game/01_Scripts/ObjectSpawner.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.Rendering;
+using UnityEngine;
+
+public class ObjectSpawner : MonoBehaviour
+{
+    [SerializeField] private float delay;
+    [SerializeField] private GameObject objectToSpawn;
+    [SerializeField] private Vector3 objectSpawnTransform;
+    private GameObject objectSpawn;
+    private float delayTemp;
+
+    private void Start()
+    {
+        delayTemp = 0;
+    }
+
+    private void Update()
+    {
+        if (objectSpawn == null)
+        {
+            if(delayTemp >= delay)
+            {
+                objectSpawn = Instantiate(objectToSpawn, objectSpawnTransform, Quaternion.identity);
+                 
+            }
+            else
+            {
+                delayTemp += Time.deltaTime;
+            }
+        }
+        else
+        {
+            delayTemp = 0;
+        }
+    }
+}
