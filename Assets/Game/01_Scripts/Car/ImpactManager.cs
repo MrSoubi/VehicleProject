019f3d41cfcca611ec0c08040f6ccb1adc94c4f1
N3crom <mathias.manenq@gmail.com>
2024-10-09 18:59:53 +0200

    try something but a lot of thing to think

--- Assets/Game/01_Scripts/Car/ImpactManager.cs
+++ Assets/Game/01_Scripts/Car/ImpactManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Events;
 
@@ -15,12 +16,19 @@ public class ImpactManager : MonoBehaviour
     [SerializeField] private PlayerLifeManager _playerLifeManager;
     [SerializeField] private float _baseImpactForceMultiplier;
     [SerializeField] private float _pourcentageMultiplier;
+    [SerializeField] [Range(0, 1)]private float _advantageMultiplier; //Le multiplicateur pour le joueur qui a l avantage dans la collision
+
+    private float lastSpeed, speedOddFrame, speedEvenFrame;
 
     private void Start()
     {
         velocityOddFrame = rb.velocity;
         velocityEvenFrame = rb.velocity;
         lastVelocity = rb.velocity;
+
+        speedOddFrame = rb.velocity.magnitude;
+        speedEvenFrame = rb.velocity.magnitude;
+        lastSpeed = rb.velocity.magnitude;
     }
 
     private void FixedUpdate()
@@ -29,21 +37,29 @@ public class ImpactManager : MonoBehaviour
         {
             velocityEvenFrame = rb.velocity;
             lastVelocity = velocityOddFrame;
+
+            speedEvenFrame = rb.velocity.magnitude;
+            lastSpeed = speedOddFrame;
         }
         else
         {
             velocityOddFrame = rb.velocity;
             lastVelocity = velocityEvenFrame;
+
+            speedOddFrame = rb.velocity.magnitude;
+            lastSpeed = speedEvenFrame;
+
         }
     }
 
     private void OnCollisionEnter(Collision collision)
     {
+        float OtherCarSpeed = collision.rigidbody.velocity.magnitude;
         ImpactManager otherCar;
         PlayerLifeManager otherCarLifeManager;
         collision.gameObject.TryGetComponent<PlayerLifeManager>(out otherCarLifeManager);
 
-        if (!collision.gameObject.TryGetComponent<ImpactManager>(out otherCar) && !collision.gameObject.TryGetComponent<PlayerLifeManager>(out otherCarLifeManager))
+        if (!collision.gameObject.TryGetComponent<ImpactManager>(out otherCar))
         {
             return;
         }
@@ -55,38 +71,57 @@ public class ImpactManager : MonoBehaviour
 
         isInvicible = true;
 
-        HandleImpact(otherCar, collision, otherCarLifeManager);
+        HandleImpact(otherCar, collision, otherCarLifeManager, OtherCarSpeed);
     }
 
-    private void HandleImpact(ImpactManager otherCar, Collision collision, PlayerLifeManager otherLifeManager)
+    private void HandleImpact(ImpactManager otherCar, Collision collision, PlayerLifeManager otherLifeManager, float OtherCarSpeed)
     {
         Vector3 impactVelocity = lastVelocity + otherCar.lastVelocity;
 
         float score_A = Vector3.Dot(impactVelocity, lastVelocity);
         float score_B = Vector3.Dot(impactVelocity, otherCar.lastVelocity);
-
+        Debug.Log("scoreA : " + score_A);
+        Debug.Log("score_B : " + score_B);
         bool hasAdvantage = score_A > score_B;
 
         StartCoroutine(nameof(InvicibilityRoutine));
-
+        
         if (!hasAdvantage)
         {
             Vector3 impactForce = rb.velocity - lastVelocity;
 
+            Debug.Log("Impact Velocity : " +impactVelocity);
+            Debug.Log("Impact force : " + impactForce);
+
+            //float OtherCarSpeed= collision.rigidbody.velocity.magnitude;
+            //OtherCarSpeed = 
+            Debug.Log(rb.velocity.magnitude);
+
+            impactForce *= _playerLifeManager.GetDamageMultiplier() * _baseImpactForceMultiplier;
+            _playerLifeManager.ApplyDamage(OtherCarSpeed * _pourcentageMultiplier);
+            //Debug.Log("Multiplier add to other car" + OtherCarSpeed * _pourcentageMultiplier);
+
+
             
-            
-            impactForce *= otherLifeManager.GetDamageMultiplier() * _baseImpactForceMultiplier;
-            otherLifeManager.ApplyDamage(collision.rigidbody.velocity.magnitude * _pourcentageMultiplier);
-            Debug.Log("Multiplier add" + collision.rigidbody.velocity.magnitude * _pourcentageMultiplier);
-            //_playerLifeManager.GetDamageMultiplier();
-            //_playerLifeManager.ApplyDamage(5);
 
 
             rb.AddForce(impactForce, ForceMode.Impulse);
+            
         }
 
         if (hasAdvantage)
         {
+            Vector3 impactForce = rb.velocity - lastVelocity;
+            //Debug.Log(rb.velocity.magnitude);
+            //float OtherCarSpeed = collision.rigidbody.velocity.magnitude;
+            //Debug.Log(OtherCarSpeed);
+            impactForce *= _playerLifeManager.GetDamageMultiplier() * _baseImpactForceMultiplier * _advantageMultiplier;
+            _playerLifeManager.ApplyDamage(OtherCarSpeed * _pourcentageMultiplier * _advantageMultiplier);
+
+            rb.AddForce(impactForce, ForceMode.Impulse);
+
+            //Debug.Log("Multiplier add" + OtherCarSpeed * _pourcentageMultiplier * _advantageMultiplier);
+
             Debug.Log(gameObject);
         }
     }
@@ -96,4 +131,10 @@ public class ImpactManager : MonoBehaviour
         yield return new WaitForSeconds(1f);
         isInvicible = false;
     }
+
+    public float GetPourcentageMultiplier()
+    {
+        return _pourcentageMultiplier;
+    }
+    
 }
--- Assets/Game/01_Scripts/Car/PlayerLifeManager.cs
+++ Assets/Game/01_Scripts/Car/PlayerLifeManager.cs
@@ -6,12 +6,20 @@ public class PlayerLifeManager : MonoBehaviour
 {
     private float _bumpLifeMultiplierPourcentage = 0f;
     [SerializeField] private float _mutiplierByBumpPourcentage;
-    
+    public bool debugActive;
     public void ApplyDamage(float amount)
     {
-        Debug.Log("PourcentageMultiplier before: " + Mathf.RoundToInt(_bumpLifeMultiplierPourcentage));
+        if (debugActive)
+        {
+            Debug.Log("PourcentageMultiplier before: " + Mathf.RoundToInt(_bumpLifeMultiplierPourcentage));
+
+        }
         _bumpLifeMultiplierPourcentage += amount;
-        Debug.Log("PourcentageMultiplier after: " + Mathf.RoundToInt(_bumpLifeMultiplierPourcentage));
+        if (debugActive)
+        {
+            Debug.Log("PourcentageMultiplier after: " + Mathf.RoundToInt(_bumpLifeMultiplierPourcentage));
+
+        }
 
     }
 
