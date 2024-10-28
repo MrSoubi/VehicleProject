381331f577771620072ab435bedb97057019d8b4
EmmanuelBouis <emmanuel.bouis@outlook.fr>
2024-10-28 12:22:41 +0100

    Fix UI not showing on split screen

--- Assets/Game/01_Scripts/UI_Scenes/S_CameraLayerSetup.cs
+++ Assets/Game/01_Scripts/UI_Scenes/S_CameraLayerSetup.cs
@@ -6,6 +6,7 @@ public class S_CameraLayerSetup : MonoBehaviour
 {
     [SerializeField] private List<GameObject> _virtualCameras;
     [SerializeField] private Camera _Camera;
+    [SerializeField] private Canvas _Canvas;
     int _layerMask = 0;
     int _playerID = 0;
     [SerializeField] int _firstPlayerLayerIndex;
@@ -30,6 +31,12 @@ public class S_CameraLayerSetup : MonoBehaviour
         _Camera.gameObject.layer = _layerMask;
         _Camera.cullingMask &= ~((1 << 7) | (1 << 8) | (1 << 9) | (1 << 10)); //Enleve les layers des joueurs sur le culling mask de la camera
         _Camera.cullingMask |= LayerMask.GetMask($"PlayerCam{_playerID + 1}"); //Rajoute le layer du joueur
+
+        _Canvas.gameObject.layer = _layerMask;
+        foreach(Transform t in _Canvas.transform)
+        {
+            t.gameObject.layer = _layerMask;
+        }
     }
 
 }
