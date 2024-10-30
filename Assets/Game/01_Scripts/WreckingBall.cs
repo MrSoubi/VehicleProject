a80df8886fc257ae03b06f7931fbef7564b53765
MartinsFlorian <f.martins2005.fm@gmail.com>
2024-10-30 18:26:12 +0100

    WreckingBall

--- Assets/Game/01_Scripts/WreckingBall.cs
+++ Assets/Game/01_Scripts/WreckingBall.cs
@@ -6,12 +6,45 @@ public class WreckingBall : MonoBehaviour
 {
     [SerializeField] private float wreckingBallForceHorizontal;
     [SerializeField] private float wreckingBallForceUp;
+    [SerializeField] private List<Transform> ballPointDirection;
+    [SerializeField] private Rigidbody rb;
+    [SerializeField] private float ballSpeed;
 
+    private Transform target;
+    private int targetIndex;
+
+    private void Start()
+    {
+        targetIndex = 0;
+        target = ballPointDirection[targetIndex];
+    }
+
+    private void Update()
+    {
+        Vector3 direction = (target.position - transform.position).normalized;
+        rb.velocity = direction * ballSpeed;
+
+        for (int i = 0; i < ballPointDirection.Count; i++)
+        {
+            if(Vector3.Distance(target.position, transform.position) <= 0.1f)
+            {
+                if(targetIndex >= ballPointDirection.Count)
+                {
+                    targetIndex = 0;
+                    target = ballPointDirection[targetIndex];
+                }
+                else
+                {
+                    targetIndex++;
+                    target = ballPointDirection[targetIndex];
+                }
+            }
+        }
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.GetComponent<CarController>() != null)
         {
-            Debug.Log("Hit");
             Vector3 dir = Vector3.ProjectOnPlane((other.transform.position - transform.position), Vector3.up).normalized;
             Rigidbody otherRB = other.GetComponent<Rigidbody>();
             otherRB.AddForce(dir * wreckingBallForceHorizontal, ForceMode.Impulse);
