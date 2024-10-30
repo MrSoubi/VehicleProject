b019989ea86626fd536e8cb022f4e2e4ecb194a8
EmmanuelBouis <emmanuel.bouis@outlook.fr>
2024-10-30 13:37:00 +0100

    Added sounds

--- Assets/Game/01_Scripts/Car/CarEngineAudio.cs
+++ Assets/Game/01_Scripts/Car/CarEngineAudio.cs
@@ -5,21 +5,30 @@ using UnityEngine;
 public class CarEngineAudio : MonoBehaviour
 {
     [SerializeField] FMOD.Studio.EventInstance CarEngine;
+    [SerializeField] FMOD.Studio.EventInstance CarCrash;
+
     float RPM;
 
     [SerializeField] CarController carController;
+    [SerializeField] ImpactManager impactManager;
 
     private void Awake()
     {
         CarEngine = FMODUnity.RuntimeManager.CreateInstance("event:/Car/Engine");
+        CarEngine = FMODUnity.RuntimeManager.CreateInstance("event:/FX/Crash");
     }
 
     private void Start()
     {
         FMODUnity.RuntimeManager.AttachInstanceToGameObject(CarEngine, carController.transform);
+        FMODUnity.RuntimeManager.AttachInstanceToGameObject(CarCrash, carController.transform);
+
         CarEngine.start();
+
+        impactManager.OnImpactAsAVictim.AddListener(() => { CarCrash.start(); });
     }
 
+    // TODO: utiliser l'event on ground pour faire diminuer le rpm si on ne touche pas le sol
     void Update()
     {
         CarEngine.setParameterByName("RPM", Mathf.Min(1, carController.GetSpeedRatio()) * 8500);
--- Assets/Game/01_Scripts/Car/ImpactManager.cs
+++ Assets/Game/01_Scripts/Car/ImpactManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class ImpactManager : MonoBehaviour
 {
@@ -17,7 +18,8 @@ public class ImpactManager : MonoBehaviour
     bool isInvicible = false;
 
     private float lastSpeed, _speedOddFrame, _speedEvenFrame;
-    
+
+    public UnityEvent OnImpactAsAVictim;
 
 
     private void Start()
@@ -55,9 +57,6 @@ public class ImpactManager : MonoBehaviour
     {
         ImpactManager otherCar;
 
-        //float ImpactVibrationValue = lastSpeed;
-        //_rumbleManager.InvokeImpactVibration(ImpactVibrationValue); //for test
-
         if (!collision.gameObject.TryGetComponent<ImpactManager>(out otherCar))
         {
             
@@ -76,9 +75,6 @@ public class ImpactManager : MonoBehaviour
 
     private void HandleImpact(ImpactManager otherCar, Collision collision)
     {
-        
-
-
         // Determination de l'avantage
         // On compare l'alignement entre la vélocité de chaque voiture avant l'impact à la direction de la somme de ces vélocités
         // La voiture dont la vélocité est la plus alignée à la somme est celle qui a l'avantage.
@@ -97,27 +93,6 @@ public class ImpactManager : MonoBehaviour
         // On lance une invincibilité après chaque impact
         StartCoroutine(nameof(InvicibilityRoutine));
 
-        // Sans avantage, on prend des dégâts et une force d'impact sur le RB
-        //if (!hasAdvantage)
-        //{
-        //    Vector3 impactForce = rb.velocity - lastVelocity;
-
-        //    // On applique des dégâts équivalents à la force de l'impact
-        //    // La force d'impact est calculée en retirant la vélocité de la dernière frame à la vélocité actuelle
-        //    _playerLifeManager.ApplyDamage(impactForce.magnitude);
-
-        //    // La force d'impact appliquée au RB est multipliée par le pourcentage de dégâts
-        //    impactForce *= _playerLifeManager.GetDamageMultiplier() * 4000;
-
-        //    rb.AddForce(impactForce, ForceMode.Impulse);
-
-        //    // Ajout d'une légère force vers le haut
-        //    rb.AddForce(Vector3.up * impactForce.magnitude / 10, ForceMode.Impulse);
-
-        //    // Ajout d'un léger torque
-        //    rb.AddTorque(impactForce / 20, ForceMode.Impulse);
-        //}
-
         if (!hasAdvantage)
         {
             Vector3 impactForce = rb.velocity - lastVelocity;
@@ -129,18 +104,14 @@ public class ImpactManager : MonoBehaviour
 
             _playerLifeManager.ApplyDamage(Mathf.Abs(lastSpeed - LastSpeedOtherCar) * _pourcentageMultiplier);
 
-            //Debug.Log("Multiplier add to other car" + Mathf.Abs(lastSpeed - LastSpeedOtherCar) * _pourcentageMultiplier);
-
-            //impactForce.y += _verticalBumpForce * LastSpeedOtherCar;
-
             rb.AddForce(impactForce, ForceMode.Impulse);
 
             rb.AddTorque(impactForce / 20, ForceMode.Impulse);
 
             Instantiate(_impactSFX, collision.transform.position, Quaternion.identity);
+
+            OnImpactAsAVictim.Invoke();
         }
-
-
         // Avec l'avantage on ne subit rien du tout
     }
 
