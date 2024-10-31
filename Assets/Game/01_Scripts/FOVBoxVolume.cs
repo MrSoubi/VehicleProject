97cd8026b1fff63a2f021e16a8106edeb0128aed
MartinsFlorian <f.martins2005.fm@gmail.com>
2024-10-31 14:13:22 +0100

    BombSpawner Prefab + Fix smooth Lens Distortion

--- Assets/Game/01_Scripts/FOVBoxVolume.cs
+++ Assets/Game/01_Scripts/FOVBoxVolume.cs
@@ -1,20 +1,29 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 
 public class FOVBoxVolume : MonoBehaviour
 {
     [SerializeField] private List<Volume> volume;
     [SerializeField] private BoostController boostController;
+    [SerializeField] private float maxValueLens;
+    [SerializeField] private float minValueLens;
 
+    private LensDistortion lensDistortion;
+    private float time = 0f;
     void Update()
     {
         if (boostController.isBoosting)
         {
             for (int i = 0; i < volume.Count; i++)
             {
-                volume[i].enabled = true;
+                volume[i].profile.TryGet(out lensDistortion);
+ 
+                lensDistortion.intensity.value = Mathf.Lerp(maxValueLens, minValueLens, time / 1f);
+                time += Time.deltaTime;
             }
 
         }
@@ -22,7 +31,9 @@ public class FOVBoxVolume : MonoBehaviour
         {
             for (int i = 0; i < volume.Count; i++)
             {
-                volume[i].enabled = false;
+                volume[i].profile.TryGet(out lensDistortion);
+                lensDistortion.intensity.value = 0;
+                time = 0;
             }
         }
     }
